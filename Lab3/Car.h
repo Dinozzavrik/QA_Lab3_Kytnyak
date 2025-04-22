d9dcce3a724a5aa5849bd69e9f7b015d80526179
Dinozzavrik <dianadi222@mail.ru>
2025-04-22 19:50:50 +0700

    Добавьте файлы проекта.

--- Lab3/Car.h
+++ Lab3/Car.h
@@ -6,7 +6,7 @@
  * @brief Класс Car представляет легковой автомобиль.
  *
  * Наследует Vehicle и добавляет информацию о мощности двигателя.
- *
+ *\image html car_example.jpg
  */
 class Car : public Vehicle {
 protected:
